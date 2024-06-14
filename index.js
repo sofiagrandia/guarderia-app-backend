6b7e53fde6cbe2e4c596b29a8838e06a0d9bd3bd
osmartinez <its.osmartinez@gmail.com>
2024-06-14 22:38:54 +0200

    commit inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Importar rutas
+const userRoutes = require('./routes/user.routes');
+const bookingRoutes = require('./routes/booking.routes');
+const vehicleRoutes = require('./routes/vehicle.routes');
+
+dotenv.config(); // Cargar variables de entorno desde .env
+const app = express();
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Conexión a MongoDB Atlas
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('Error connecting to MongoDB', err));
+
+// Definir rutas
+app.use('/api/users', userRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/vehicles', vehicleRoutes);
+
+// Definir el puerto y poner en marcha el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
